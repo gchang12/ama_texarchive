03cc86b64e5754fb147ad4ca0ff2613f1bbefffa
gchang12 <gc8@hawaii.edu>
2024-03-11 21:40:59 -0700

    not yet tested.

---
+++ finalize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# TeX-escape pound-sign
+#echo "Please escape pound-sign";
+#read;
+pound_file=output/ama_text/"Aaron Hammersley"/IWantFries21/answer_text.txt;
+sed -i "s/\\#/\\\\#/" $pound_file;
+# generate metadata
+cd output/ama_text/;
+ls --sort=time --reverse > ../../metadata/content-creators.txt;
+# to manually reorder content creator names, if applicable
+#vi ../../metadata/content-creators.txt;
+cd output/ama_text/;
+mkdir filenames;
+# generate list of fan names
+for cc_name in *; do (
+    ls --sort=time --reverse "$cc_name" > filenames/"$cc_name".txt;
+); done;
+# moving last of metadata to right directory
+rm filenames/filenames.txt;
+mv filenames ../../metadata/;
+cd ../../;
+# typeset, and do it twice for indexing purposes
+pdflatex main -interaction nonstopmode
+    && pdflatex main -interaction nonstopmode;
